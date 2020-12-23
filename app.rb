f869f6294b13b146f534950eb115b28b68ab2325
Trayshawn <traytwebb14@yahoo.com>
2020-12-23 16:26:42 -0600

    commit tree 9ee9099dda81549f5f94f40ce5e53406b02b1073

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do 
+        enable :session
+        set :sessions_secrect, SecureRandom.hex
+    end 
+
+    get '/' do 
+        erb :index
+    end 
+
+    post '/checkout' do 
+        session[:item] = params[:item]
+        @session = session 
+    end 
+
+
+
 end
